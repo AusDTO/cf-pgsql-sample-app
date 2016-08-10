04b32d9c8089189161e9ea4d18082201a2bc725c
Angie Gove <angie.gove@betterbills.com>
2016-08-10 15:50:32 +1000

    need app name to set env variable, put it back

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,6 @@ cf api ${CF_API}
 cf auth ${CF_STAGING_USER} ${CF_STAGING_PASSWORD}
 cf target -o ${CF_ORG} -s ${CF_SPACE}
 
-cf set-env CI_PULL_REQUEST ${CI_PULL_REQUEST}
 # step 2. is this a PR?
 if [ -z "${CI_PULL_REQUEST:-}" ] ; then
 	echo "commit is not part of a pull request, skipping deploy"
@@ -21,15 +20,13 @@ fi
 # step 3. create and app name for this branch
 
 CF_APP_NAME="$CIRCLE_USERNAME:$CIRCLE_BRANCH" | md5sum | cut -f 1 -d ' '
-cf set-env CF_APP_NAME ${CF_APP_NAME}
 CF_SERVICE_NAME=${CF_APP_NAME}-db
 
 # step 4. create db service and app for this branch
 cf push ${CF_APP_NAME} --no-start
-
+cf set-env ${CF_APP_NAME} CI_PULL_REQUEST ${CI_PULL_REQUEST}
 cf create-service dto-shared-pgsql shared-psql ${CF_SERVICE_NAME}
 cf bind-service ${CF_APP_NAME} ${CF_SERVICE_NAME}
-
 # step 5. send generated url to github
 curl -X POST --data '{"body":"here is your url "}' -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/users/technoweenie -I
 echo "check git comments if it is posting test all the things"
