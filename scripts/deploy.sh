38308cb9b6ab7a23aa0a66c09cb66ea3e8218733
Angie Gove <angie.gove@betterbills.com>
2016-08-10 14:24:09 +1000

    use oauth token to speak to github, create unique 32 max character name for app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,20 +17,20 @@ if [ -z "${CI_PULL_REQUEST:-}" ] ; then
 	echo "commit is not part of a pull request, skipping deploy"
 	exit 0
 fi
+# step 3. create and app name for this branch
 
-# step 3. create db service and app for this branch
-CF_APP_NAME=${CIRCLE_PROJECT_REPONAME}-$(git rev-parse --short ${CIRCLE_SHA1})
-CF_SERVICE_NAME=${CIRCLE_PROJECT_REPONAME}-$(git rev-parse --short ${CIRCLE_SHA1})-db
+CF_APP_NAME="$CIRCLE_USERNAME:$CIRCLE_BRANCH" | md5sum | cut -f 1 -d ' '
+CF_SERVICE_NAME=${CF_APP_NAME}-db
 
-cf push ${CF_APP_NAME} --no-start --random-route
+# step 4. create db service and app for this branch
+cf push ${CF_APP_NAME} --no-start
 cf set-env ${CF_APP_NAME} CI_PULL_REQUEST ${CI_PULL_REQUEST}
 cf create-service dto-shared-pgsql shared-psql ${CF_SERVICE_NAME}
 cf bind-service ${CF_APP_NAME} ${CF_SERVICE_NAME}
 
-#send generated url to github
-curl -X POST --data '{"body":"here is your url"}' https://api.github.com/repos/AusDTO/cf-pgsql-sample-app/issues/1/comments
+# step 5. send generated url to github
+curl -X POST --data '{"body":"here is your url "}' -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/users/technoweenie -I
 echo "check git comments if it is posting test all the things"
 
-
-# step 4. fire!
+# step 6. fire!
 cf start ${CF_APP_NAME}
