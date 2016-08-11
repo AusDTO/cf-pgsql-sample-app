1d9c23ace7dc875fe8a11250e06fc94f9516a508
Angie Gove <angie.gove@betterbills.com>
2016-08-11 12:18:49 +1000

    more testing of syntax evaluating command in if statement

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,29 +19,24 @@ if [ -z "${CI_PULL_REQUEST:-}" ] ; then
 fi
 
 #step 4. don't create another db or app if they already exist
-if [ -z "${CI_PULL_REQUEST:-}" ] ; then
-	echo "commit is not part of a pull request, skipping deploy"
-	exit 0
-fi
-
-SCRIPT=$(cf app ${CF_APP_NAME})
-if [ $? == "FAILED"] ; then
+if (( $(cf app ${CF_APP_NAME}) == "FAILED" )) ; then
 	echo "it worked"
 else
 	echo "no it didnt, gosh"
 fi
-# # step 3. create and app name for this branch
-# GITHUB_USER_BRANCH=${CIRCLE_USERNAME}:${CIRCLE_BRANCH}
-# CF_APP_NAME=$(${GITHUB_USER_BRANCH}| md5sum | cut -f 1 -d ' ')
-# CF_SERVICE_NAME=${CF_APP_NAME}-db
-#
-# # step 4. create db service and app for this branch
-# cf push ${CF_APP_NAME} --no-start
-# cf set-env ${CF_APP_NAME} CI_PULL_REQUEST ${CI_PULL_REQUEST}
-# cf create-service dto-shared-pgsql shared-psql ${CF_SERVICE_NAME}
-# cf bind-service ${CF_APP_NAME} ${CF_SERVICE_NAME}
-# # step 5. send generated url to github
-# curl -H "Authorization: token ${GITHUB_TOKEN}" --data '{ "body":"'"here is your url https://${CF_APP_NAME}.apps.staging.digital.gov.au"'"}' https://api.github.com/repos/AusDTO/cf-pgsql-sample-app/issues/1/comments
-#
-# # step 6. fire!
-# cf start ${CF_APP_NAME}
+
+# step 3. create and app name for this branch
+GITHUB_USER_BRANCH=${CIRCLE_USERNAME}:${CIRCLE_BRANCH}
+CF_APP_NAME=$(${GITHUB_USER_BRANCH}| md5sum | cut -f 1 -d ' ')
+CF_SERVICE_NAME=${CF_APP_NAME}-db
+
+# step 4. create db service and app for this branch
+cf push ${CF_APP_NAME} --no-start
+cf set-env ${CF_APP_NAME} CI_PULL_REQUEST ${CI_PULL_REQUEST}
+cf create-service dto-shared-pgsql shared-psql ${CF_SERVICE_NAME}
+cf bind-service ${CF_APP_NAME} ${CF_SERVICE_NAME}
+# step 5. send generated url to github
+curl -H "Authorization: token ${GITHUB_TOKEN}" --data '{ "body":"'"here is your url https://${CF_APP_NAME}.apps.staging.digital.gov.au"'"}' https://api.github.com/repos/AusDTO/cf-pgsql-sample-app/issues/1/comments
+
+# step 6. fire!
+cf start ${CF_APP_NAME}
