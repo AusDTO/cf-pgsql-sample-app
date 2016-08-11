e58e949dc77eec1c8bb0326bee5991bb37ccf2ca
Angie Gove <angie.gove@betterbills.com>
2016-08-11 16:48:50 +1000

    Go back to check as the evaluation results in different thing each time

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,17 +26,18 @@ CF_SERVICE_NAME=${CF_APP_NAME}-db
 #step 4. don't create another db or app if they already exist
 cf app ${CF_APP_NAME} && cf service ${CF_SERVICE_NAME}
 if [ $? -eq 0 ]; then
-	cf push ${CF_APP_NAME}
+	echo "This needs to only be pushed"
 else
-	# step 4. create db service and app for this branch
-	cf push ${CF_APP_NAME} --no-start
-	cf set-env ${CF_APP_NAME} CI_PULL_REQUEST ${CI_PULL_REQUEST}
-	cf create-service dto-shared-pgsql shared-psql ${CF_SERVICE_NAME}
-	cf bind-service ${CF_APP_NAME} ${CF_SERVICE_NAME}
-	
-	# step 5. send generated url to github
-	curl -H "Authorization: token ${GITHUB_TOKEN}" --data '{ "body":"'"here is your url https://${CF_APP_NAME}.apps.staging.digital.gov.au"'"}' https://api.github.com/repos/AusDTO/cf-pgsql-sample-app/issues/1/comments
+  echo "this needs to create a app and db"
+fi # we want to create a new one only if app has been created and db doesn't exist
 
-	# step 6. fire!
-	cf start ${CF_APP_NAME}
-fi
+# # step 4. create db service and app for this branch
+# cf push ${CF_APP_NAME} --no-start
+# cf set-env ${CF_APP_NAME} CI_PULL_REQUEST ${CI_PULL_REQUEST}
+# cf create-service dto-shared-pgsql shared-psql ${CF_SERVICE_NAME}
+# cf bind-service ${CF_APP_NAME} ${CF_SERVICE_NAME}
+# # step 5. send generated url to github
+# curl -H "Authorization: token ${GITHUB_TOKEN}" --data '{ "body":"'"here is your url https://${CF_APP_NAME}.apps.staging.digital.gov.au"'"}' https://api.github.com/repos/AusDTO/cf-pgsql-sample-app/issues/1/comments
+#
+# # step 6. fire!
+# cf start ${CF_APP_NAME}
