4a5ba49520403255740f849d7238161ef46c1ce1
Angie Gove <angie.gove@betterbills.com>
2016-08-11 17:02:31 +1000

    cf check service does not respond with an 0 exit code but only text so change if statement to evaluate on not equal as it fails with exit code  like cf app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,19 +25,19 @@ CF_SERVICE_NAME=${CF_APP_NAME}-db
 
 #step 4. don't create another db or app if they already exist
 cf app ${CF_APP_NAME} && cf service ${CF_SERVICE_NAME}
-if [ $? -eq 0 ]; then
-	echo "This needs to only be pushed"
-else
-  echo "this needs to create a app and db"
-fi # we want to create a new one only if app has been created and db doesn't exist
+if [ $? -ne 0 ]; then
+	echo "build all the things"
+	# step 4. create db service and app for this branch
+	cf push ${CF_APP_NAME} --no-start
+	cf set-env ${CF_APP_NAME} CI_PULL_REQUEST ${CI_PULL_REQUEST}
+	cf create-service dto-shared-pgsql shared-psql ${CF_SERVICE_NAME}
+	cf bind-service ${CF_APP_NAME} ${CF_SERVICE_NAME}
+	# step 5. send generated url to github
+	curl -H "Authorization: token ${GITHUB_TOKEN}" --data '{ "body":"'"here is your url https://${CF_APP_NAME}.apps.staging.digital.gov.au"'"}' https://api.github.com/repos/AusDTO/cf-pgsql-sample-app/issues/1/comments
 
-# # step 4. create db service and app for this branch
-# cf push ${CF_APP_NAME} --no-start
-# cf set-env ${CF_APP_NAME} CI_PULL_REQUEST ${CI_PULL_REQUEST}
-# cf create-service dto-shared-pgsql shared-psql ${CF_SERVICE_NAME}
-# cf bind-service ${CF_APP_NAME} ${CF_SERVICE_NAME}
-# # step 5. send generated url to github
-# curl -H "Authorization: token ${GITHUB_TOKEN}" --data '{ "body":"'"here is your url https://${CF_APP_NAME}.apps.staging.digital.gov.au"'"}' https://api.github.com/repos/AusDTO/cf-pgsql-sample-app/issues/1/comments
-#
-# # step 6. fire!
-# cf start ${CF_APP_NAME}
+	# step 6. fire!
+	cf start ${CF_APP_NAME}  # if it doesnt exist create all the things
+else
+	echo "only push"
+  cf push #only push
+fi
